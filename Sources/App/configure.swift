291675d4bbe733f3ac9ed6c7a134d8c7d6366866
Matthew <h.mattjunk@gmail.com>
2024-10-24 03:19:10 +0800

    chore: first routes

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,9 +1,41 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) async throws {
+    
+    // Load the SSL certificate
+    let certificatePath = app.directory.resourcesDirectory + "ca-certificate.crt"
+    let certificate = try NIOSSLCertificate(file: certificatePath, format: .pem)
+    
+    // Create an SSL context configuration
+    var sslContextConfig = TLSConfiguration.makeClientConfiguration()
+    sslContextConfig.certificateVerification = .fullVerification
+    sslContextConfig.trustRoots = .certificates([certificate])
+    
+    // Create SSL context
+    let sslContext = try NIOSSLContext(configuration: sslContextConfig)
+    
+    app.databases.use(
+        .postgres(
+            configuration: .init(
+                hostname: Environment.get("PG_DB_HOSTNAME")!,
+                port: Int(Environment.get("PG_DB_PORT")!) ?? 25060,
+                username: Environment.get("PG_DB_USERNAME")!,
+                password: Environment.get("PG_DB_PASSWORD")!,
+                database: Environment.get("PG_DB_DATABASE")!,
+                tls: .require(sslContext)
+            )
+        ),
+        as: .psql
+    )
+    
+    app.migrations.add(CreateGalaxy())
+    
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
     // register routes
     try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,10 +2,21 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async in
-        "It works!"
+        "It works normally!"
     }
 
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("planet") { req async throws in
+        try await Planet.query(on: req.db).all()
+    }
+    
+    app.post("planet") { req async throws -> Planet in
+        let galaxy = try req.content.decode(Planet.self)
+        try await galaxy.create(on: req.db)
+        return galaxy
+    }
+
 }
